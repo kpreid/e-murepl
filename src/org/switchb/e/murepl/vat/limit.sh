95466d48ae2b594962f5779193958725fa4a6b83
Kevin Reid <kpreid@switchb.org>
2011-05-14 14:17:38 -0600

    Add copyright boilerplate.

--- src/org/switchb/e/murepl/vat/limit.sh
+++ src/org/switchb/e/murepl/vat/limit.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Copyright 2011 Kevin Reid, under the terms of the MIT X license
+# found at http://www.opensource.org/licenses/mit-license.html ................
+
 # Usage: limit.sh [-l <ulimit option> <ulimit value>]* -- <classpath> <limitSub args>
 
 while [ "X$1" = 'X-l' ]; do
