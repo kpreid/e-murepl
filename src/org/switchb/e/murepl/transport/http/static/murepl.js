4d5efe2e0c775f2169e6424a5a2efd7cbad72371
Kevin Reid <kpreid@switchb.org>
2011-07-05 14:32:38 -0700

    Move HTTP REPL script and style into static files served by Jetty.
    Also added that typing without the input field focused focuses it.

---
+++ src/org/switchb/e/murepl/transport/http/static/murepl.js
@@ -0,0 +1,32 @@
+// Copyright 2011 Kevin Reid, under the terms of the MIT X license
+// found at http://www.opensource.org/licenses/mit-license.html ...............
+
+"use strict";
+
+function ajax() {
+  var src = $('#input').get(0).value;
+  var entry = $("#out").append("<div><pre>? <span class='input'></span></pre></div>").children().last();
+  entry.find(".input").text(src);
+  var responseHolder = entry.append("<div>...evaluating...</div>").children().last();
+  responseHolder.load('/repl', {src: src, noout: 1}, function (response, status, xhr) {
+    if (status == "error") {
+      responseHolder.html("Error contacting server: " + xhr.status + " " + xhr.statusText);
+    }
+    $(document.body).scrollTop(responseHolder.position().top - 10);
+  });
+  $("#input").select();
+}
+
+function murepl_init() {
+  $("#input").focus();
+  
+  $(document).bind("keydown", function (e) {
+    // Put focus on the input field if any key is pressed
+    //   ...unless it's just a modifier state change
+    //   ...or a keyboard shortcut (we're assuming that shortcuts have at least
+    //      one of alt, ctrl, meta)
+    if (!(e.altKey || e.ctrlKey || e.metaKey || e.keyCode == 0 || e.keyCode == 16)) {
+      $("#input").focus();
+    }
+  });
+}
