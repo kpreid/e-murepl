698d483734ff2e6be975c673f5b772359a6d930f
Kevin Reid <kpreid@switchb.org>
2011-08-21 11:58:31 -0600

    Add long-polling for web UI

--- src/org/switchb/e/murepl/transport/http/static/murepl.js
+++ src/org/switchb/e/murepl/transport/http/static/murepl.js
@@ -3,34 +3,67 @@
 
 "use strict";
 
+var mureplNetStatus;
+
 function ajax() {
   var src = $('#input').get(0).value;
 
-  var responseHolder = $("<div><pre>? <span class='input'></span></pre><div>...evaluating...</div></div>")
-  responseHolder.find(".input").text(src);
-  $('#out').append(responseHolder);
-  
-  responseHolder.load('', {
-    src: src, 
-    noout: 1, 
-    log: $('#form input[name="log"]').val()
-  }, function (response, status, xhr) {
-    if (status == "error") {
-      responseHolder.html("Error contacting server: " + xhr.status + " " + xhr.statusText);
-    }
-    
-    // grab logmark data supplied by server to use on next request
-    $('#form input[name="log"]').remove();
-    $('#form').append($('#logmark input'));
-    $('#logmark').remove();
-    
-    $(document.body).scrollTop(responseHolder.position().top - 10);
+  $.ajax('', {
+    type: 'POST',
+    data: {
+      src: src, 
+      output: "none"
+    },
+    error: function (xhr, status, httpError) {
+      $('#out').append("<div>Error sending code to server: " + status + " " + httpError + "</div>");
+    },
   });
   $("#input").get(0).value = "";
   $("#input").select();
 }
 
+var longPollFailures = 0;
+
+function lp(responseHolder) {
+  if (!responseHolder) {
+    responseHolder = $("<div></div>")
+    $('#out').append(responseHolder);
+  }
+  
+  responseHolder.load('', {
+    output: "log",
+    wait: 1,
+    log: $('#form input[name="log"]').val()
+  }, function (response, status, xhr) {
+    if (status == "error") {
+      // Exponential backoff
+      var delaySec = Math.pow(2, ++longPollFailures);
+
+      mureplNetStatus.text("Network error: " + xhr.status + ". Retrying in " + delaySec + " s.");
+      
+      setTimeout(function () { lp(responseHolder); },
+                 delaySec * 1000);
+    } else {
+      // grab logmark data supplied by server to use on next request
+      $('#form input[name="log"]').remove();
+      $('#form').append($('#logmark input'));
+      $('#logmark').remove();
+      
+      // start next long-poll immediately
+      longPollFailures = 0;
+      lp();
+    }
+
+    // TODO: only do this if already at bottom/user evaled something
+    $(document.body).scrollTop(responseHolder.position().top - 10);
+  });
+
+  mureplNetStatus.text("Ready");
+}
+
 function murepl_init() {
+  mureplNetStatus = $('#netstatus');
+
   $("#input").focus();
   
   $(document).bind("keydown", function (e) {
@@ -42,4 +75,6 @@ function murepl_init() {
       $("#input").focus();
     }
   });
+  
+  setTimeout(lp, 0);
 }
